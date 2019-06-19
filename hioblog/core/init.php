9f31deecd7b90ca737ca5f983ade0e0ec22ca299
aakash dhar <daxeffect007@gmail.com>
2019-06-20 02:36:21 +0530

    removed db strings

--- hioblog/core/init.php
+++ hioblog/core/init.php
@@ -1,7 +1,7 @@
 <?php
 
   session_start();
-  $con = mysqli_connect('localhost','root','kittywolf','master_db');
+  $con = mysqli_connect('localhost','','','master_db');
 
   if (!$con) {
     die('There was some issue connecting to the database we are trying to fix this ASAP');
